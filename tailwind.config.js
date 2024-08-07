347d13a838bac3d3e00df50ec4714223ecbc4d69
farhanScripts <farhan.dev06@gmail.com>
2024-08-07 18:03:07 +0700

    adding landing page & login page

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,6 +3,18 @@ module.exports = {
   content: ['./src/**/*.{html,js}'],
   theme: {
     extend: {
+      gridTemplateRows: {
+        footer: '200px 400px',
+      },
+      animation: {
+        'loop-scroll': 'loop-scroll 50s linear infinite',
+      },
+      keyframes: {
+        'loop-scroll': {
+          from: { transform: 'translateX(0)' },
+          to: { transform: 'translateX(-100%)' },
+        },
+      },
       fontFamily: {
         poppins: ['Poppins', 'sans-serif'],
         dmsans: ['DM Sans', 'sans-serif'],
@@ -11,9 +23,11 @@ module.exports = {
         softBeige: '#F5F5DC',
         richGreen: '#264F0B',
         brightYellow: '#FFD700',
+        lightGrey: '#E5E5E5',
       },
       backgroundImage: {
         'hero-pattern': "url('assets/pexels-gabby-k-5273676.jpg')",
+        men_collection: "url('assets/men_collection_photo.jpg')",
       },
     },
   },
